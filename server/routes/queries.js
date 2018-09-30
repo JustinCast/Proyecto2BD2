98673769c4c5028885c2bdc0251dc59f069cd9d3
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-09-29 22:32:48 -0600

    feat: servidor funcionando

--- server/routes/queries.js
+++ server/routes/queries.js
@@ -1,31 +1,24 @@
-var promise = require("bluebird");
+const pg = require("pg");
 
-var options = {
-  // Initialization Options
-  promiseLib: promise
-};
-
-const cn = {
-  host: "jc-postgresql.postgres.database.azure.com",
-  port: 5432,
-  database: "p2db2",
-  user: "JustinCast",
-  password: "python.1524"
-};
-
-var pgp = require("pg-promise")(options);
-/*const connectionString =
-  "postgres://JustinCast@jc-postgresql:python.1524@jc-postgresql.postgres.database.azure.com:5432/p2db2?ssl=true";*/
-//var db = pgp(cn);
-
+//var pool;
 function login(req, res) {
-  cn.host = req.body.server;
-  cn.port = req.body.port;
-  cn.database = req.body.database;
-  cn.user = req.body.user;
-  cn.password = req.body.password;
-  db = pgp(cn);
+  try {
+    const client = new pg.Client({
+      connectionString:
+        "postgres://JustinCast@jc-postgresql:python.1524@jc-postgresql.postgres.database.azure.com:5432/p2db2?ssl=true"
+    });
+    client.connect();
 
+    client
+      .query("SELECT NOW()")
+      .then(res => {
+        console.log(res);
+        client.end();
+      })
+      .catch(err => console.log(err));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
